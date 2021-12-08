e7b52332169ab3e6392c82897feb1ac0e86ca515
SuryaKarmakar <surjokarmakarssj7@gmail.com>
2021-12-08 22:17:02 +0530

    send normal html and json response

--- server.js
+++ server.js
@@ -1,9 +1,33 @@
 const http = require("http");
 
 const PORT = 8000;
+const data = [
+  {
+    id: 1,
+    name: "user 1",
+    age: 21,
+  },
+  {
+    id: 2,
+    name: "user 2",
+    age: 23,
+  },
+  {
+    id: 3,
+    name: "user 3",
+    age: 20,
+  },
+  {
+    id: 4,
+    name: "user 4",
+    age: 27,
+  },
+];
 
 const requestListener = (req, res) => {
-  res.write("<h1>Welcome to vanilla node server</h1>");
+  //   res.write("<h1>Welcome to vanilla node server</h1>");
+  //   res.end();
+  res.write(JSON.stringify(data));
   res.end();
 };
 const server = http.createServer(requestListener);
