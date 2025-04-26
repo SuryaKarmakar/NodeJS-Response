6517402ed57c58337c04f5aaae15cb403e6c91de
Surya Karmakar <74616729+SuryaKarmakar@users.noreply.github.com>
2025-04-26 14:22:23 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,26 +1,10 @@
 const http = require("http");
 
 const data = require("./data");
-let fs = require("fs");
 
 const PORT = 8000;
 
 const requestListener = (req, res) => {
-  // res.writeHead(200, { "Content-Type": "text/html" });
-  // res.write("<h1>Welcome to vanilla node server</h1>");
-  // res.end();
-
-  // fs.readFile("./index.html", null, (error, data) => {
-  //   if (error) {
-  //     res.writeHead(404);
-  //     res.write("Whoops! File not found!");
-  //   } else {
-  //     res.writeHead(200, { "Content-Type": "text/html" });
-  //     res.write(data);
-  //   }
-  //   res.end();
-  // });
-
   res.writeHead(200, { "Content-Type": "application/json" });
   res.write(JSON.stringify(data));
   res.end();
